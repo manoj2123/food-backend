a3a0e5fe57aada146e0561b79d7fe7a867b149c9
manoj2123 <manojkumarga2103@gmail.com>
2023-07-19 18:14:18 +0530

    token changed

--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,11 @@ import { client } from "../db.js";
 import Jwt from "jsonwebtoken";
 
 export function addUser(userInfo) {
-  return client.db("cars").collection("users").insertOne(userInfo);
+  return client.db("recipe").collection("users").insertOne(userInfo);
 }
 
 export function getUser(userEmail) {
-  return client.db("cars").collection("users").findOne({ email: userEmail });
+  return client.db("recipe").collection("users").findOne({ email: userEmail });
 }
 
 export function generateJwtToken(id) {
